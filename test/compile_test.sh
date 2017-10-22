ae3f8573ecdd860867d25a7bb7d4e6a17d05bb0e
M. George Hansen <technopolitica@gmail.com>
2017-10-22 16:23:44 -0700

    Add test script ala heroku-buildpack-testrunner to make sure that shared libs are installed

---
+++ test/compile_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+test_shared_libs() {
+    compile
+
+    . ${BUILDPACK_HOME}/export
+    cat <<EOF > "${BUILD_DIR}/test.c"
+#include <wkhtmltox/pdf.h>
+
+int main()
+{
+    wkhtmltopdf_init(0);
+    wkhtmltopdf_deinit();
+
+    return 0;
+}
+EOF
+    capture gcc "${BUILD_DIR}/test.c" -o "${BUILD_DIR}/test" -lwkhtmltox
+    assertCapturedSuccess
+
+    capture "${BUILD_DIR}/test"
+    assertCapturedSuccess
+}
